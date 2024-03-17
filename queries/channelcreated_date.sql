8245b07c369ffd922b37137da705c91e98097f10
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2024-03-17 06:36:56 +0600

    Update query

--- queries/channelcreated_date.sql
+++ queries/channelcreated_date.sql
@@ -1,11 +1,14 @@
 SELECT 
-json_extract(data, '$.aboutChannelViewModel.canonicalChannelUrl') AS canonicalChannelUrl,
+  json_extract(data, '$.aboutChannelViewModel.canonicalChannelUrl') AS canonicalChannelUrl,
   json_extract(data, '$.aboutChannelViewModel.joinedDateText.content') AS joinedDate,
   json_extract(data, '$.aboutChannelViewModel.country') AS country,
   json_extract(data, '$.aboutChannelViewModel.subscriberCountText') AS subscriberCountText,
   json_extract(data, '$.aboutChannelViewModel.viewCountText') AS viewCountText,
+  CAST(REPLACE(substr(json_extract(data, '$.aboutChannelViewModel.viewCountText'), 1, instr(json_extract(data, '$.aboutChannelViewModel.viewCountText'), ' ') - 1), ',', '') AS INTEGER) AS viewCount,
   json_extract(data, '$.aboutChannelViewModel.videoCountText') AS videoCountText,
+  CAST(REPLACE(substr(json_extract(data, '$.aboutChannelViewModel.videoCountText'), 1, instr(json_extract(data, '$.aboutChannelViewModel.videoCountText'), ' ') - 1), ',', '') AS INTEGER) AS videoCount,
   json_extract(data, '$.aboutChannelViewModel.description') AS description
 FROM channel_details
-WHERE joinedDate like '%2023%' and country like '%united states%'
-;
+WHERE canonicalChannelUrl is not NULL
+and videoCount<101
+and viewCount>50000
--- queries/subscribercount.sql
+++ queries/subscribercount.sql
@@ -1,10 +1,12 @@
 SELECT *,
-       SUBSTR(json_extract(data, '$.videoCountText.simpleText'), 1, INSTR(json_extract(data, '$.videoCountText.simpleText'), ' ') - 1) AS videoCount,
+       SUBSTR(json_extract(data, '$.videoCountText.simpleText'), 1, INSTR(json_extract(data, '$.videoCountText.simpleText'), ' ') - 1) AS SubscribersCount,
        CASE
          WHEN SUBSTR(json_extract(data, '$.videoCountText.simpleText'), 1, INSTR(json_extract(data, '$.videoCountText.simpleText'), ' ') - 1) like '%k%' THEN
            CAST(REPLACE(SUBSTR(json_extract(data, '$.videoCountText.simpleText'), 1, LENGTH(json_extract(data, '$.videoCountText.simpleText')) - 1), ',', '') AS REAL) * 1000
          ELSE
            CAST(REPLACE(SUBSTR(json_extract(data, '$.videoCountText.simpleText'), 1, INSTR(json_extract(data, '$.videoCountText.simpleText'), ' ') - 1), ',', '') AS REAL)
-       END AS numeric_videoCount
+       END AS numeric_SubscribersCount
+	   
 FROM channels
-WHERE videoCount NOT LIKE '%M%' and numeric_videoCount>=20001 and numeric_videoCount<=40000 and channels.channelId not in (SELECT channelId from channel_details);
+WHERE SubscribersCount NOT LIKE '%M%' and numeric_SubscribersCount>=2500
+and channelId not in (Select channelId from channel_details);
