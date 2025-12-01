965107ac118fefdbf095ae49b0162f1c63ee3fdd
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2025-12-01 07:32:57 +0600

    # Changes to be committed: #	deleted:    api/search.ipynb #	deleted:    channel_details-from search.py #	deleted:    channel_details-from socialblade.py #	deleted:    continuation_search.txt #	deleted:    queries/channelcreated_date.sql #	deleted:    queries/export.sql #	deleted:    queries/subscribercount.sql #	deleted:    search_channel.py #	deleted:    socialblade_similar_crawler.py #	deleted:    timedata.py #	deleted:    videos_crawler.py #

--- queries/export.sql
+++
@@ -1,6 +0,0 @@
-SELECT *, MIN(timestamp),max(timestamp) FROM youtube_videos 
-LEFT JOIN timedata on
-youtube_videos.videoId=timedata.videoId
-LEFT JOIN channel_details_parsed on
-youtube_videos.channelId=channel_details_parsed.channelId
-GROUP by youtube_videos.channelId
